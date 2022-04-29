3dce50020890d5c857e4f69f2aa53472e19866fe
kreangkrai <kreangkrai.ratt@gmail.com>
2022-04-29 17:12:56 +0700

    update 2022-04-29

--- www/index.js
+++ www/index.js
@@ -1,25 +1,25 @@
 import init ,{Point} from "rust-wasm-dev"
 var React = require('react');
 init().then(_ =>{
-    const point = new Point(3,4);
-
-    const add = point.mul();
-
-    console.log(add);
-    //getapi();
+    getData('http://127.0.0.1:8082/datas' )
+    .then(data => {
+     
+      const point = new Point(data[0].value,data[1].value);
+      const add = point.add();
+      console.log(add);
+    })
+    .catch(e =>{
+      console.log(e);
+    });
 
 });
 
-// async function getapi() {
-//     const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrcmlhbmdrcmFpQGdtYWlsLmNvbSIsIm5hbWUiOiJrcmlhbmdrcmFpIiwicm9sZSI6IkFkbWluIiwiaWF0IjoxNjUwNDI1MDYzMTA3LCJleHAiOjE2NTMwMTcwNjMxMDd9.Ndbx0Ou1MDhfPFyypQVxwBRtbJI4lkvN7jYZHv4mpqI';
-//     const response = await fetch("http://localhost:8082/products",{
-//         method: "GET",
-//         mode: 'no-cors',
-//         headers: new Headers({
-//             'Content-Type' : 'applications/json',
-//             'Authorization' : `Bearer ${token}`
-//         })
-//     });
-//     var data = await response.json();
-//     console.log(data);
-// }
+async function getData(url = '') {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',      
+      },
+    });
+    return response.json();
+  }
